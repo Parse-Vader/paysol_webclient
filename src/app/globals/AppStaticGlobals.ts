ef047f1ae402e95b287232a21ace95b0834d593a
Parse-Vader <79918104+Parse-Vader@users.noreply.github.com>
2024-04-13 16:51:09 +0200

    sa

--- src/app/globals/AppStaticGlobals.ts
+++ src/app/globals/AppStaticGlobals.ts
@@ -1,17 +1,18 @@
 export class AppStaticGlobals {
-  static Data: string = "";
-  static Nonce: string = "";
-  static pub_key: string = "";
-  static session: string = "";
-  static phantom_encryption_public_key: string = "";
+  static Data: string = '';
+  static Nonce: string = '';
+  static pub_key: string = '';
+  static session: string = '';
+  static phantom_encryption_public_key: string = '';
   static redirect: boolean = false;
 
+
   static DisConnect(){
-    AppStaticGlobals.Data = "",
-    AppStaticGlobals.Nonce = "",
-    AppStaticGlobals.pub_key = "",
-    AppStaticGlobals.session = "",
-    AppStaticGlobals.phantom_encryption_public_key = "",
+    AppStaticGlobals.Data = '',
+    AppStaticGlobals.Nonce = '',
+    AppStaticGlobals.pub_key = '',
+    AppStaticGlobals.session = '',
+    AppStaticGlobals.phantom_encryption_public_key = '',
     AppStaticGlobals.redirect = false
   }
 }

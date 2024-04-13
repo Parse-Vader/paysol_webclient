0ade05b099220adeb4a083261ebb67d9fcb65765
Parse-Vader <79918104+Parse-Vader@users.noreply.github.com>
2024-04-13 16:52:18 +0200

    ads

--- src/app/globals/AppStaticGlobals.ts
+++ src/app/globals/AppStaticGlobals.ts
@@ -1,9 +1,9 @@
 export class AppStaticGlobals {
-  static Data: string = "";
-  static Nonce: string = "";
-  static pub_key: string = "";
-  static session: string = "";
-  static phantom_encryption_public_key: string = "";
+  static Data: string = '';
+  static Nonce: string = '';
+  static pub_key: string = '';
+  static session: string = '';
+  static phantom_encryption_public_key: string = '';
   static redirect: boolean = false;
-  static txNanoId: string = "";
+  static txNanoId: string = '';
 }

0934a5d3935888bb50a60d28d97896814fb6757f
esataydin <esat61_2001@hotmail.com>
2024-04-19 20:47:33 +0200

    s

--- src/app/services/cookies.service.ts
+++ src/app/services/cookies.service.ts
@@ -2,20 +2,13 @@ import { Injectable } from '@angular/core';
 import * as nacl from 'tweetnacl';
 import * as crypto from 'crypto';
 import {CookieService} from "ngx-cookie-service";
-import { ncrypt } from "ncrypt-js"
-import {Keypair} from "@solana/web3.js";
-import {ClientRequestService} from "./client-request.service";
-import {AppStaticGlobals} from "../globals/AppStaticGlobals";
-import {TransactionModel} from "../interfaces/transaction.model";
-import {Contract} from "../interfaces/contract.enum";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookiesService {
-  public _secretKey: string = '';
-  constructor(private cookieService: CookieService, private _clientRequest: ClientRequestService) {}
+  constructor(private cookieService: CookieService) {}
 
   public setTxNanoIdCookie(txNanoId: string): void {
     this.cookieService.set('nanocookie', txNanoId);
@@ -24,18 +17,9 @@ export class CookiesService {
   public getTxNanoIdCookie(): string {
     return this.cookieService.get('nanocookie');
   }
-
-  public setNaclBoxKeyPair(keyPair: nacl.BoxKeyPair) {
-    // const _secretkey = this._clientRequest.getSecretKey();
-    this._clientRequest.getSecretKey().subscribe(
-      (data: string) => {
-        this._secretKey = data;
-        const {encrypt, decrypt} = new ncrypt(this._secretKey);
-        const encryptedKeypair = encrypt(this._secretKey);
-        this.cookieService.set('paysolcookie', encryptedKeypair);
-      },
-      (error) => console.error(error)
-    );
+  public setNaclBoxKeyPair(keyPair: nacl.BoxKeyPair): void {
+    const serializedKeyPair = JSON.stringify(keyPair);
+    this.cookieService.set('paysolcookie', serializedKeyPair);
   }
   public getNaclBoxKeyPair(): nacl.BoxKeyPair {
     const cookieValue = this.cookieService.get('paysolcookie');
